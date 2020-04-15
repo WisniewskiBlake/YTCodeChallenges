47d824c072a5dd07c86f5b1a46283a4da81e7139
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-04-15 05:03:25 -0400

    testing 20x20

--- NQueens/NQueens.java
+++ NQueens/NQueens.java
@@ -4,6 +4,12 @@ package NQueens;
  * NQueens
  */
 public class NQueens {
-
+    private int[][] chessTable;
+	private int numOfQueens;
+	
+	public NQueens(int numOfQueens) {
+		this.chessTable = new int[numOfQueens][numOfQueens];
+		this.numOfQueens = numOfQueens;
+	}
     
 }
